db31a1f1f241625885c4ccba5df7a292778627e6
w2cluster121@iiht.tech <w2cluster121@iiht.tech>
2020-04-28 09:41:44 +0530

    Lab demo 1st MS

--- src/main/java/com/currency/CurrencyDemoApplication.java
+++ src/main/java/com/currency/CurrencyDemoApplication.java
@@ -1,13 +1,22 @@
 package com.currency;
 
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableEurekaClient
 @SpringBootApplication
+@EnableFeignClients
 public class CurrencyDemoApplication {
-
+	
+	private static final Logger logger = LoggerFactory.getLogger(CurrencyDemoApplication.class);
 	public static void main(String[] args) {
-		System.out.println("hello check");
+		logger.info("In Main SpringBoot class ->CurrencyDemoApplication");
 		SpringApplication.run(CurrencyDemoApplication.class, args);
 	}
 
--- src/main/java/com/currency/controller/CurrencyController.java
+++ src/main/java/com/currency/controller/CurrencyController.java
@@ -3,8 +3,9 @@ package com.currency.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,38 +17,52 @@ public class CurrencyController {
 	
 	@Autowired
 	CurrencyExchangeDAO daoExchange;
+	private static final Logger logger = LoggerFactory.getLogger(CurrencyController.class);
 	
 	@RequestMapping("/addCurrency")
 	public String addCurrency(CurrencyExchange objExchange)
 	{
-		System.out.println("hello world");
-		daoExchange.save(objExchange);
-		System.out.println("hello world out");
-		return "added Currency  ";
+		String status = "";
+		logger.info("endpoint /addCurrency -> Entry");
+		try {
+			daoExchange.save(objExchange);
+			status = "data added";
+		}catch(Exception e)
+		{
+			logger.error(e.getStackTrace().toString());
+			status ="error in data to be added";
+		}
+		
+		logger.info("endpoint /addCurrency -> Exit");
+		return status;
 	}
 	
 	@RequestMapping("/updateCurrency")
 	public String updateCurrency(CurrencyExchange objExchange)
 	{
+		String status = "Nothing to update";
 		CurrencyExchange objExchangeFromDB = null;
-		System.out.println("hello world");
+		logger.info("endpoint /updateCurrency -> Entry");
 		boolean isExists = daoExchange.existsById(objExchange.getCountryCode());
 		if(isExists)
 		{
 			objExchangeFromDB = daoExchange.findById(objExchange.getCountryCode()).get();
 			objExchangeFromDB.setConversionRate(objExchange.getConversionRate());
 			daoExchange.save(objExchangeFromDB);
+			status ="Updated Currency Exchange rate";
+			
 		}
-		System.out.println("hello world out");
-		return "hello world  no currencyfound to update";
+		logger.info("endpoint /updateCurrency -> Exit");
+		return status;
 	}
 	
 	@RequestMapping("/getCurrency")
 	public List<CurrencyExchange> getExchangeRate(String countryCode)
 	{
+		logger.info("endpoint /getCurrency -> Entry");
+		
 		List<CurrencyExchange> objExchangeFromDB = new  ArrayList();
 		
-		System.out.println("hello world");
 		if (countryCode != null )
 		{
 			boolean isExists = daoExchange.existsById(countryCode);
@@ -65,32 +80,35 @@ public class CurrencyController {
 			objsExchange.forEach(objExchangeFromDB::add);
 			
 		}
-		System.out.println("hello world out");
+		
+		logger.info("endpoint /getCurrency -> exit");
+		
 		return objExchangeFromDB;
 		
 	}
 	
 	@RequestMapping("/convertCurrency")
-	public double convertCurrency(String countryCode,double amount)
+	public double convertCurrency(String countryCode,Double amount)
 	{
 		CurrencyExchange objExchangeFromDB = null;
 		double exchangedAmount = 0.0;
+		logger.info("endpoint /convertCurrency -> Entry code " + countryCode +"amount = "+amount);
 		
-		System.out.println("in  convertCurrency");
 		if (countryCode != null )
 		{
+			System.out.println("in not null country code");
 			boolean isExists = daoExchange.existsById(countryCode);
 			if(isExists)
 			{
 				objExchangeFromDB = daoExchange.findById(countryCode).get();
 				exchangedAmount = objExchangeFromDB.getConversionRate()*amount;
-				
+				System.out.println("in not null country code" + exchangedAmount + "and value from db = "+exchangedAmount);
 				
 			}
 		}
 		
 		
-		System.out.println("out  convertCurrency");
+		logger.info("endpoint /convertCurrency -> exit");
 		return exchangedAmount;
 		
 	}
